3ebb1a16cc37284b3877d86484fe3d9a72933f1f
Gonçalo Ferreira <goncalo_c_ferreira@hotmail.com>
2025-11-09 04:51:33 +0000

    Move Sudoku assets to public directory
    All Sudoku-related files have been relocated from the sudoku/ directory to public/sudoku/ to improve asset organization and ensure proper static file serving. Also added src/Sudoku.css and updated src/Sudoku.js for related changes.

--- src/Sudoku.js
+++ src/Sudoku.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Sudoku.css';
 
 const Sudoku = () => {
   return (
-    <div>
-      <h1>Sudoku Game</h1>
-      {/* Your Sudoku game implementation will go here */}
+    <div className="sudoku-container">
+      <div className="sudoku-header">
+        <h1>Sudoku Game</h1>
+        <Link to="/" className="back-link">← Back to Portfolio</Link>
+      </div>
+      <div className="game-wrapper">
+        <iframe
+          src="/sudoku/Sudoku.html"
+          title="Sudoku Game"
+          className="game-iframe"
+        ></iframe>
+      </div>
     </div>
   );
 };
