ee22e96ef624e205b131f365132a808be6d4ed52
Gonçalo Ferreira <goncalo_c_ferreira@hotmail.com>
2025-11-11 01:31:46 +0000

    Update Sudoku web app assets and cache version
    Updated Sudoku.pck asset and its file size reference in Sudoku.html. Modified viewport meta tag and removed safe-area-inset paddings for improved layout. Bumped service worker cache version to ensure clients receive updated resources.

--- public/sudoku/Sudoku.service.worker.js
+++ public/sudoku/Sudoku.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1762824387|2004309278';
+const CACHE_VERSION = '1762824670|2287203272';
 /** @type {string} */
 const CACHE_PREFIX = 'Sudoku-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
